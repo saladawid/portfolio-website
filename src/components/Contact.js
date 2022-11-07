4ccb3e209b6bfda1285caf5a2e5815394bbcb762
saladawid <dawidsalamon@gmail.com>
2022-11-07 08:35:56 +0100

    Refactored website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Contact = () => {
+    const [form, setForm] = useState({
+        name: '', email: '', message: '',
+    });
+    const [require, setRequire] = useState(false);
+    const [invalid, setInvalid] = useState(null);
+    const [summary, setSummary] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const sendForm = async (e) => {
+        e.preventDefault();
+
+        if (!(!form.name || !form.email || !form.message)) {
+            if (!form.email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)) {
+                setInvalid('Invalid email');
+            } else {
+                setLoading(true);
+                const res = await fetch("https://getform.io/f/f5729be5-83d4-4c44-b1a8-e837fb2e1286", {
+                    method: 'POST', headers: {
+                        'Content-Type': 'application/json',
+                    }, body: JSON.stringify({
+                        ...form,
+                    }),
+                });
+
+                if (res.ok) {
+                    setLoading(false);
+                    setSummary('Thank you for your message');
+                }
+            }
+        } else {
+            setRequire(true);
+        }
+    };
 
     return (
         <div name="contact" className="w-full h-screen bg-[#0d0d0d] flex justify-center items-center text-center p-4">
-            <form method="POST" action="https://getform.io/f/f5729be5-83d4-4c44-b1a8-e837fb2e1286"
+            <form onSubmit={sendForm}
                   className="flex flex-col max-w-[600px] w-full">
                 <p className="my-3 text-3xl sm:text-7xl font-bold text-[#327361]">Contact</p>
-                <input className="p-2" type="text" placeholder="Name" name="name" required/>
-                <input className="my-3 p-2" type="email" placeholder="Email" name="email" required/>
-                <textarea className="p-2" rows="10" placeholder="Message" name="message" required></textarea>
-                <button
-                    className="text-white group border-2 px-6 py-2 my-4 mx-auto flex hover:bg-[#327361] hover:border-[#327361] duration-300">
-                    Send message
+                <input
+                    className={!form.name && require ? 'border-[3px] border-amber-500 placeholder-amber-500 p-2' : 'border-[3px] p-2 bg-[white]'}
+                    type="text" placeholder={(!form.name && require) ? "Please complete the field" : "Name"}
+                    name="name" value={form.name}
+                    onChange={e => setForm(form => ({
+                        ...form, name: e.target.value,
+                    }))}
+                    disabled={summary}
+                />
+                <input
+                    className={!form.email && require ? 'border-[3px] border-amber-500 placeholder-amber-500 my-3 p-2' :
+                        invalid ? 'border-[3px] border-amber-500 placeholder-amber-500 my-3 text-amber-500 p-2 ' : 'input-email border-[3px] my-3 p-2 bg-[white] fill-amber-200'}
+                    type="email"
+                    placeholder={(!form.email && require) ? "Please complete the field" : "Email"}
+                    onFocus={() => setInvalid(null)}
+                    name="email" value={invalid ?? form.email}
+                    onChange={e => setForm(form => ({
+                        ...form, email: e.target.value,
+                    }))}
+                    disabled={summary}
+                />
+                <textarea
+                    className={!form.message && require ? 'border-[3px] border-amber-500 placeholder-amber-500 p-2' : 'box-border border-[3px] p-2 bg-[white]'}
+                    rows="10" placeholder={(!form.message && require) ? "Please complete the field" : "Message"}
+                    name="message"
+                    value={form.message}
+                    onChange={e => setForm(form => ({
+                        ...form, message: e.target.value,
+                    }))}
+                    disabled={summary}
+                ></textarea>
+                <button onClick={sendForm}
+                        className={!summary && !loading ? "text-white group border-2 px-6 py-2 my-4 mx-auto flex hover:bg-[#327361] hover:border-[#327361] duration-300" : "text-white group border-2 px-6 py-2 my-4 mx-auto flex bg-[#327361] border-[#327361] duration-300"}
+                        disabled={summary || loading}
+                >
+                    {loading ? "Sending" : summary ? summary : "Send message"}
                 </button>
             </form>
-        </div>
-    );
+        </div>);
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import CV from '../assets/Dawid_Salamon_CV.pdf';
+import CV from '../assets/Dawid Salamon__CV.pdf';
 import {IoIosArrowForward} from 'react-icons/io';
 
 export const Home = () => {
     return (
         <div name="home" className="w-full h-screen bg-[#f2f2f2]">
-            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-                <p className="text-3xl sm:text-7xl font-bold text-[#d98014]">I warmly welcome, I am</p>
-                <h1 className="text-3xl sm:text-7xl mb-3 font-bold text-[#0d0d0d]">Dawid Salamon</h1>
+            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center sm:text-left text-center h-full">
+                <h1 className="text-3xl sm:text-7xl sm:mt-2 mt-6 font-bold text-[#d98014]">I warmly welcome</h1>
+                <h2 className="text-3xl sm:text-7xl mb-5 font-bold text-[#0d0d0d]">My name is Dawid</h2>
                 <h2 className="text-2xl sm:text-4xl font-bold text-[#0d0d0d]">I have been learning JavaScript for two
                     years</h2>
-                <h2 className="text-2xl sm:text-4xl mt-3 font-bold text-[#d98014]">I would love to become a Junior Node.js
+                <h2 className="text-2xl sm:text-4xl mt-4 font-bold text-[#d98014]">I would love to become a Junior Node.js
                     Developer</h2>
                 <p className="text-2xl sm:text-4xl mt-3 font-bold text-[#0d0d0d]">Technologies that I learn and use:</p>
-                <p className="text-2xl sm:text-4xl my-3 font-bold text-[#d98014]">Node.js, Express, React, MongoDB,
-                    MySQL</p>
+                <span className="text-2xl sm:text-4xl my-3 font-bold text-[#d98014]">Node.js, <span>Express,</span> React, MongoDB,
+                    MySQL</span>
                 <div>
                     <button
                         className="text-white bg-[#0d0d0d] group border-2 px-6 py-2 my-4 hover:bg-[#F28F16] hover:border-[#F28F16] duration-300">
